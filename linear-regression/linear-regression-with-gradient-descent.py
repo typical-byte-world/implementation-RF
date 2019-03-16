a7a8255d17d7d6ac8d01ab0f9d7dd1c61ed2a8c1
Artem <artem@gmail.com>
2019-03-16 20:24:37 +0200

    new linear regression with gradient descent

---
+++ linear-regression/linear-regression-with-gradient-descent.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+# def compute_error_for_given_points(b, m ,data):
+#     total_err = 0
+#     for i in range(len(data)):
+#         x = data[i, 0]
+#         y = data[i, 1]
+#         total_err += (y - (m * x + b)) ** 2
+#     return total_err / float(len(data))
+
+
+def step_gradient(b_current, m_current, points, learningRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    new_b = b_current - (learningRate * b_gradient)
+    new_m = m_current - (learningRate * m_gradient)
+    return [new_b, new_m]
+
+
+
+def gradien_descent_runner(data, start_b, start_m, learning_rate, num_iterations):
+    b = start_b
+    m = start_m
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, data, learning_rate)
+
+    return [b, m]
+def run():
+    data = np.genfromtxt('data.csv', delimiter=',')
+    learning_rate = 0.0001
+    #y = mx + b
+    init_m = 0
+    init_b = 0
+    num_iterations = 1000
+    print(f"Intial value of b is {init_b}.\nInitial value of m is {init_m}\nIterations {num_iterations}")
+
+    [m, b] = gradien_descent_runner(data, init_b, init_m, learning_rate, num_iterations)
+    print(f"\nAfter {num_iterations} iterations end value of b is {b} and value of m is {m}")
+
+
+if __name__ == '__main__':
+    run()
+
